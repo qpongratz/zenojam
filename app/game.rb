4595615b1f8e2b43bde2b3850b86314c866df4d4
Quentin Pongratz <qpongratz@gmail.com>
2024-04-30 13:03:26 -0500

    fix ball clipping into side of bricks

--- app/game.rb
+++ app/game.rb
@@ -4,10 +4,13 @@ class Game
   def game args
     args.state.ball_damage ||= 1
     args.state.bricks_left ||= 1000000000
+    args.state.brick_width ||= 50
+    args.state.brick_height ||= 20
     args.state.paddle ||= { x: 100, y: 100, w: 120, h: 10, r: 0, g: 0, b: 0 }
 
-    args.state.ball ||= { x: 640, y: 360, w: 10, h: 10, path: 'sprites/circle/orange.png' }
+    set_quadrant_angles args
 
+    args.state.ball ||= { x: 640, y: 360, w: 10, h: 10, path: 'sprites/circle/orange.png' }
 
     args.state.bricks ||= []
     if args.state.tick_count == 0 || args.state.bricks.empty?
@@ -17,7 +20,7 @@ class Game
           x = 50 + (i * 60)
           y = 650 - (j * 30)
           next if rand(2) == 0
-          args.state.bricks << Brick.new(x: x, y: y, w: 50, h: 20, health: (rand(7) + 1) )
+          args.state.bricks << Brick.new(x: x, y: y, w: args.state.brick_width, h: args.state.brick_height, health: (rand(7) + 1) )
         end
       end
     end
@@ -32,7 +35,6 @@ class Game
       args.state.paddle.x += 10
     end
 
-
     if args.state.ball.intersect_rect? args.state.paddle
       args.state.ball_y_direction *= -1
       calculate_new_ball_position args
@@ -46,12 +48,25 @@ class Game
       args.state.ball_y_direction *= -1
     end
 
-    args.state.bricks.each_with_index do |brick, index|
-      next unless args.state.ball.intersect_rect? brick
+    brick = args.geometry.find_intersect_rect args.state.ball, args.state.bricks
+    if brick
+      brick_center = { x: brick.x + (brick.w / 2), y: brick.y + (brick.h / 2) }
+      ball_center = { x: args.state.ball.x + (args.state.ball.w / 2), y: args.state.ball.y + (args.state.ball.h / 2) }
+      test_angle = brick_center.angle_to ball_center
+      target_x = args.state.ball_x_direction.positive? ? 180 : 0
+      target_y = args.state.ball_y_direction.positive? ? 270 : 90
+      if Geometry.angle_within_range? test_angle, target_x, args.state.vertical_quadrant_angle
+        args.state.ball_x_direction *= -1
+        args.state.move_ball = false
+      end
+
+      if Geometry.angle_within_range? test_angle, target_y, args.state.horizontal_quadrant_angle
+        args.state.ball_y_direction *= -1
+        args.state.move_ball = false
+      end
       brick.take_damage(args.state.ball_damage)
-      args.state.bricks.delete_at index if brick.health <= 0
+      args.state.bricks.delete(brick) if brick.health <= 0
       args.state.bricks_left -= 1
-      args.state.ball_y_direction *= -1
     end
 
     calculate_new_ball_position args
@@ -62,6 +77,14 @@ class Game
     args.outputs.sprites << args.state.bricks
     args.outputs.solids << args.state.paddle
     args.outputs.sprites << args.state.ball
+  end
+
+  def set_quadrant_angles args
+    args.state.horizontal_quadrant_angle ||= (
+      Math.atan((args.state.brick_width / 2) / (args.state.brick_height / 2)) * 180 / Math::PI
+    )
+
+    args.state.vertical_quadrant_angle ||= (90 - args.state.horizontal_quadrant_angle)
   end
 
   def move_ball args
