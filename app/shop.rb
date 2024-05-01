f704f30e7f8617061b158e2e78f6fc9e4d7ce994
Quentin Pongratz <qpongratz@gmail.com>
2024-05-01 18:33:41 -0500

    hook up cost to upgrades

--- app/shop.rb
+++ app/shop.rb
@@ -1,27 +1,30 @@
 class Shop
   def menu args
 
-  # args.gtk.log upgrades: args.state.upgrades
+  args.state.upgrades = [
+      Upgrade.new(label_text: "Ball Speed", cost: 5, proc: -> { args.state.ball_speed += 1 }),
+      Upgrade.new(label_text: "Brick Health Mult", cost: 5, proc: -> { args.state.brick_health_multiplier += 1 }),
+      Upgrade.new(label_text: "Ball Damage", cost: 10, proc: -> { args.state.ball_damage += 1 })
+    ]
 
-    # args.state.ball_speed ||= 5
-    args.state.upgrades ||= []
+    args.state.buttons ||= []
 
-    if args.state.upgrades.empty?
-      args.state.upgrades << Upgrade.new(x: 100, y: 100, w: 200, h: 200, r: 255, g: 0, b: 0, **upgrades_hash(args).ball_speed)
-      args.state.upgrades << Upgrade.new(x: 400, y: 100, w: 200, h: 200, r: 255, g: 0, b: 0, **upgrades_hash(args).brick_health)
-      args.state.upgrades << Upgrade.new(x: 700, y: 100, w: 200, h: 200, r: 255, g: 0, b: 0, **upgrades_hash(args).ball_damage)
+    if args.state.buttons.empty?
+      args.state.upgrades.each_with_index do |upgrade, index|
+        args.state.buttons << Button.new(x: (100 + 250 * index), y: 100, w: 200, h: 200, r: 255, g: 0, b: 0, label_text: upgrade.label_text, proc: upgrade)
+      end
     end
 
 
-    args.state.upgrades.each do |upgrade|
-      args.outputs.labels << upgrade.label
-      args.outputs.borders << upgrade
+    args.state.buttons.each do |button|
+      args.outputs.labels << button.label
+      args.outputs.borders << button
     end
 
     if args.inputs.mouse.click
-      args.state.upgrades.each do |upgrade|
+      args.state.buttons.each do |upgrade|
         next unless args.inputs.mouse.inside_rect? upgrade
-        upgrade.call
+        upgrade.call args
         break
       end
     end
@@ -31,17 +34,29 @@ class Shop
     args.outputs.labels << [640, 600, "Ball Damage: #{args.state.ball_damage}", 5, 1, 0, 0, 0]
   end
 
-  def upgrades_hash args
-    {
-      ball_speed: { label_text: "Ball Speed", cost: 5, proc: -> { args.state.ball_speed += 1 }},
-      brick_health: { label_text: "Brick Health Mult", cost: 5, proc: -> { args.state.brick_health_multiplier += 1 }},
-      ball_damage: { label_text: "Ball Damage", cost: 10, proc: -> { args.state.ball_damage += 1 } }
-      # "paddle_width" => { name: "Paddle Width", cost: 30, proc: -> { args.state.upgrade_3_count += 1 } }
-    }
+  def upgrades args
+
   end
 end
 
 class Upgrade
+  attr_accessor :label_text, :cost, :proc
+
+  def initialize(label_text:, cost:, proc:)
+    @label_text = label_text
+    @cost = cost
+    @proc = proc
+  end
+
+  def call args
+    return if cost > args.state.wallet
+
+    args.state.wallet -= cost
+    proc.call
+  end
+end
+
+class Button
   attr_accessor :x, :y, :w, :h, :path, :angle, :a, :r, :g, :b, :tile_x,
   :tile_y, :tile_w, :tile_h, :flip_horizontally,
   :flip_vertically, :angle_anchor_x, :angle_anchor_y, :id,
@@ -50,7 +65,7 @@ class Upgrade
   :source_x2, :source_y2, :source_x3, :source_y3, :x2, :y2, :x3, :y3,
   :anchor_x, :anchor_y
 
-  def initialize(x:, y:, w:, h:, r:, g:, b:, label_text:, proc:, cost: 0)
+  def initialize(x:, y:, w:, h:, r:, g:, b:, label_text:, proc:)
     self.x = x
     self.y = y
     self.w = w
@@ -60,11 +75,10 @@ class Upgrade
     self.b = b
     @proc = proc
     @label_text = label_text
-    @cost = cost
   end
 
-  def call
-    @proc.call
+  def call(...)
+    @proc.call(...)
   end
 
   # omg the work we'll have to do to render labels that fit into a box and in the right
